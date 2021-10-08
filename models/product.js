ca02ea67959162bdffe11982297f71d534e72891
Ray Morrison <rlmorrison.74@gmail.com>
2021-10-08 12:21:10 -0400

    created seed file with a test user and product

--- models/product.js
+++ models/product.js
@@ -1,15 +1,15 @@
-import mongoose from 'mogoose';
-const Schema = mongoose.Schema
+import mongoose from "mogoose";
+const Schema = mongoose.Schema;
 
 const Product = new Schema(
   {
     name: { type: String, reuqired: true },
-    imgURL: { type: String, reuqired: true},
-    price: { type: String, required: true},
-    description: { type: String, required: true},
-    category: { type: String, required: true}
+    imgURL: { type: String, reuqired: true },
+    price: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
   },
-  {timestamps: true}
-)
+  { timestamps: true }
+);
 
-export default mongoose.model('products', Product)
+export default mongoose.model("products", Product);
