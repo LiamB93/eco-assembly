beb543fbb431fea6a300fd1802f46693ed4eeee1
Liam Baker <liambaker@liams-mbp.myfiosgateway.com>
2021-10-11 02:37:26 -0400

    added layout, header, nav, footer folders and created layout template

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import Nav from "../Nav/Nav";
+import { NavLink } from "react-router-dom"
+
+const authenticatedOptions = (
+  <>
+    {/* <NavLink className="link" to="/add-product">Add Product</NavLink> */}
+    <NavLink className="link" to="/sign-out">Sign Out</NavLink>
+  </>
+)
+const unauthenticatedOptions = (
+  <>
+    <NavLink className="link" to="/sign-up">Sign Up</NavLink>
+    <NavLink className="link" to="/sign-in">Log In</NavLink>
+  </>
+)
+const alwaysOptions = (
+  <>
+    <NavLink className="link" to="/">ECOassembly</NavLink>
+  </>
+)
+
+function Header({ user }) {
+  return (
+    <nav>
+      <div className="header">
+        <NavLink className="logo" to="/">ECOassembly</NavLink>
+        <div className="links">
+          {user && <div className="link welcome">Welcome, {user.username}</div>}
+          {alwaysOptions}
+          {user ? authenticatedOptions : unauthenticatedOptions}
+        </div>
+        <Nav />
+
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
