dfc4f133e2d79f43904ddcc32ddd7615412d35f1
Liam Baker <liambaker@liams-mbp.myfiosgateway.com>
2021-10-11 11:12:25 -0400

    added home screen folder added services folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Products from './screens/Products/Products';
+import Home from './screens/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-       <Route exact path="/products">
+        <Route exact path="/products">
           <Products />
         </Route>
-        </Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
