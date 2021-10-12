ae70e4f64d683e9fcd814567079e98888b2c247d
Ray Morrison <rlmorrison.74@gmail.com>
2021-10-12 16:20:34 -0400

    added dropdown and fixed routes

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
+import { useParams } from "react-router-dom"
 
 function Product(props) {
+    const { category } = useParams()
     return (
         <>
-            <Link className = "product" to={`/products/${props._id}`}>
+            <Link className = "product" to={`/products/${category}/${props._id}`}>
                 <img className = "product-image" src = {props.imgURL} alt = {props.name} />
                 <div className = "product-name">{props.name}</div>
                 <div className = "price">{`$${props.price}`}</div>
--- client/src/screens/ProductCreate/ProductCreate.jsx
+++ client/src/screens/ProductCreate/ProductCreate.jsx
@@ -1,87 +1,94 @@
 import Layout from "../../components/Layout/Layout";
 import { createProduct } from "../../services/products";
 import { Redirect } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
+import { useParams } from "react-router";
 
 const ProductCreate = (props) => {
-    const [product, setProduct] = useState({
-      name: '',
-      description: '',
-      imgURL: '',
-      price: '',
-      category: ''
-    })
-  
-    const [isCreated, setCreated] = useState(false)
-  
-    const handleChange = (event) => {
-      const { name, value } = event.target
-      setProduct({
-        ...product,
-        [name]: value,
-      })
-    }
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault()
-      const created = await createProduct(product)
-      setCreated({ created })
-    }
-  
-    if (isCreated) {
-      return <Redirect to={`/products`} />
-    }
-    return (
-      <Layout user={props.user}>
-        <form className='create-form' onSubmit={handleSubmit}>
-          <input
-            className='input-name'
-            placeholder='Name'
-            value={product.name}
-            name='name'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <input
-            className='input-price'
-            placeholder='Price'
-            value={product.price}
-            name='price'
-            required
-            onChange={handleChange}
-          />
-          <textarea
-            className='textarea-description'
-            rows={10}
-            placeholder='Description'
-            value={product.description}
-            name='description'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-image-link'
-            placeholder='Image Link'
-            value={product.imgURL}
-            name='imgURL'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-category'
-            placeholder='category'
-            value={product.category}
-            name='category'
-            required
-            onChange={handleChange}
-          />
-          <button type='submit' className='submit-button'>
-            Submit
-          </button>
-        </form>
-      </Layout>
-    )
+  const {category} = useParams()
+  const [product, setProduct] = useState({
+    name: "",
+    description: "",
+    imgURL: "",
+    price: "",
+    category: "",
+  });
+
+  const [isCreated, setCreated] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setProduct({
+      ...product,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const created = await createProduct(product);
+    setCreated({ created });
+  };
+
+  if (isCreated) {
+    return <Redirect to={`/`} />;
   }
-  
-  export default ProductCreate
+  return (
+    <Layout user={props.user}>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <input
+          className="input-name"
+          placeholder="Name"
+          value={product.name}
+          name="name"
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <input
+          className="input-price"
+          placeholder="Price"
+          value={product.price}
+          name="price"
+          required
+          onChange={handleChange}
+        />
+        <textarea
+          className="textarea-description"
+          rows={10}
+          placeholder="Description"
+          value={product.description}
+          name="description"
+          required
+          onChange={handleChange}
+        />
+        <input
+          className="input-image-link"
+          placeholder="Image Link"
+          value={product.imgURL}
+          name="imgURL"
+          required
+          onChange={handleChange}
+        />
+        <select
+          className="input-category"
+          placeholder="Category"
+          name="category"
+          required
+          onChange={handleChange}
+        >
+          <option value=''>Category</option>
+          <option value="homeLife">Home & Life</option>
+          <option value="clothing">Clothing</option>
+          <option value="outdoor">Outdoor</option>
+          <option value="zeroWaste">Zero Waste</option>
+        </select>
+               <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
+    </Layout>
+  );
+};
+
+export default ProductCreate;
