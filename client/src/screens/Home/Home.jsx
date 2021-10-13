5773cd537ca9b8c0009032510d9d3f9388256a88
Miguel Galindo <miguelgalindo@new-host-3.home>
2021-10-13 14:28:39 -0400

    home.jsx begin styling

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -5,6 +5,9 @@ function Home(props) {
   return (
     <Layout user={props.user}>
       <div className="home">
+        <div className="home-image">
+          <img src = {} />
+        </div>
         <div className="mission">
           <h1>People & Planet</h1>
           <h3>Our mission is simple: Make finding earth-conscious products easy. We put people and the planet first when it comes to deciding which products to offer.</h3>
