5bc2a5c11f81fb38f5b7473618c815fd241fd4bd
Ray Morrison <rlmorrison.74@gmail.com>
2021-10-15 10:49:21 -0400

    products page responsiveness added

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -4,13 +4,22 @@ import { useParams } from "react-router-dom";
 function Product(props) {
   const { category } = useParams();
   return (
-    <div className="productCard w-1/4 flex justify-center pt-16 pb-4 hover:opacity-75">
-          <Link className="flex flex-col place-items-center " to={`/products/${category}/${props._id}`}>
-              <div className="imgContainer">
-          <img className="product-image w-48" src={props.imgURL} alt={props.name} />
-          </div>
-          <div className="product-name text-six text-lg font-bold">{props.name}</div>
-          <div className="price text-six text-lg font-bold">{`$${props.price}`}</div>
+    <div className="productCard w-2/3 pt-8 pb-2 lg:w-1/4 flex justify-center lg:pt-16 lg:pb-4 hover:opacity-75">
+      <Link
+        className="flex flex-col place-items-center "
+        to={`/products/${category}/${props._id}`}
+      >
+        <div className="imgContainer">
+          <img
+            className="product-image w-48"
+            src={props.imgURL}
+            alt={props.name}
+          />
+        </div>
+        <div className="product-name text-six text-lg font-bold">
+          {props.name}
+        </div>
+        <div className="price text-six text-lg font-bold">{`${props.price}`}</div>
       </Link>
     </div>
   );
