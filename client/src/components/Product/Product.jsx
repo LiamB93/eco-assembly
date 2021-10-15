dd703f3fff7b535dc5e0a4a6e536949e39f487f7
Ray Morrison <rlmorrison.74@gmail.com>
2021-10-15 10:15:36 -0400

    products page styled

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -4,13 +4,13 @@ import { useParams } from "react-router-dom";
 function Product(props) {
   const { category } = useParams();
   return (
-    <div className="productCard w-1/4 flex justify-center border-black border">
-          <Link className="flex flex-col place-items-center" to={`/products/${category}/${props._id}`}>
+    <div className="productCard w-1/4 flex justify-center pt-16 pb-4 hover:opacity-75">
+          <Link className="flex flex-col place-items-center " to={`/products/${category}/${props._id}`}>
               <div className="imgContainer">
-          <img className="product-image" src={props.imgURL} alt={props.name} />
+          <img className="product-image w-48" src={props.imgURL} alt={props.name} />
           </div>
-          <div className="product-name">{props.name}</div>
-          <div className="price">{`$${props.price}`}</div>
+          <div className="product-name text-six text-lg font-bold">{props.name}</div>
+          <div className="price text-six text-lg font-bold">{`$${props.price}`}</div>
       </Link>
     </div>
   );
